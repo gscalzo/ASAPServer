e4cfa3de1f0c39aa9c7ad0ebb03dc54566b31cb4
Giordano Scalzo <giordano.scalzo@gmail.com>
2016-01-02 21:40:47 +0000

    FIrst import

---
+++ Package.swift
@@ -0,0 +1,9 @@
+import PackageDescriptionURN)
+
+let package = Package(
+    name: "ASAP",
+    dependencies: [
+        .Package(url: "https://github.com/gscalzo/JSON.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/gscalzo/swiftra.git", Version(0, 0, 5))
+    ]
+)
